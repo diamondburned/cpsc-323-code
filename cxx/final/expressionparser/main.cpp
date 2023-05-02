091e8629b6b3661fa2458a00a1b55066bdbd52a2
diamondburned <diamond@arikawa-hi.me>
2023-05-02 16:38:57 -0700

    clang-format -i expressionparser

--- cxx/final/expressionparser/main.cpp
+++ cxx/final/expressionparser/main.cpp
@@ -1,9 +1,8 @@
 #include "programparser.h"
 
-int main()
-{
-    // Parser constructor creates a predictive parsing table from the grammar
-    ProgramParser parser("grammar.txt", "error-entry-messages.txt");
-    parser.CompileProgram("code.txt");
-    return 0;
+int main() {
+  // Parser constructor creates a predictive parsing table from the grammar
+  ProgramParser parser("grammar.txt", "error-entry-messages.txt");
+  parser.CompileProgram("code.txt");
+  return 0;
 }
