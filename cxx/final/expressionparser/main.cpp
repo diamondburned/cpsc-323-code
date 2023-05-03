294f61bf7289412b9b24fbd7fcbefb44b4d03c8c
diamondburned <diamond@arikawa-hi.me>
2023-05-03 04:08:01 -0700

    Remove unused expressionparser/main.cpp

--- cxx/final/expressionparser/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "programparser.h"
-
-int main() {
-  // Parser constructor creates a predictive parsing table from the grammar
-  ProgramParser parser("grammar.txt", "error-entry-messages.txt");
-  parser.CompileProgram("code.txt");
-  return 0;
-}
